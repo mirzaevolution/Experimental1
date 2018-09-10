74a555b3b0b55c8e2c0f7c8153a8bd2d16bce90c
Mirza Ghulam Rasyid <mirzaevolution@gmail.com>
2018-09-10 12:58:05 +0700

    finalizing sample with url feedback

--- Experimental1/Controllers/UploadHandlerController.cs
+++ Experimental1/Controllers/UploadHandlerController.cs
@@ -3,9 +3,12 @@
 using Experimental1.Models;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 namespace Experimental1.Controllers
@@ -13,24 +16,34 @@ namespace Experimental1.Controllers
     public class UploadHandlerController : ApiController
     {
         [Route("UploadHandler/Submit")]
-        public IHttpActionResult Upload(string id="")
+        public async Task<IHttpActionResult> Upload()
         {
+           
             var files = HttpContext.Current.Request.Files;
             if (files!=null)
             {
+                var server = HttpContext.Current.Server;
+                string basePath = server.MapPath("~/Uploads");
+                if (Directory.Exists(basePath))
+                {
+                    Directory.CreateDirectory(basePath);
+                }
+                MultipartFormDataStreamProvider provider = new MultipartFormDataStreamProvider(basePath);
+                await Request.Content.ReadAsMultipartAsync(provider);
+
                 UploadedFile file = new UploadedFile()
                 {
-                    ID = id
+                    ID = provider.FormData["id"] == null ? Guid.NewGuid().ToString("n") : provider.FormData["id"].ToString()
                 };
                 int len = files.Count;
-                string basePath = HttpContext.Current.Server.MapPath("~/Uploads");
-
+            
                 for(int i = 0; i < len; i++)
                 {
                     file.FileName = files[i].FileName;
                     file.Size = files[i].ContentLength;
                     file.ContentType = files[i].ContentType;
-                    string fullname = Path.Combine(basePath, file.FileName);
+                    string virtualFullname = "~/Uploads/" + file.FileName;
+                    string fullname = server.MapPath(virtualFullname);
                     if (File.Exists(fullname))
                         File.Delete(fullname);
                     try
@@ -41,9 +54,10 @@ namespace Experimental1.Controllers
                     {
                         file.Error = ex.Message;
                     }
-                    if (!string.IsNullOrEmpty(file.Error))
+                    if (string.IsNullOrEmpty(file.Error))
                     {
-                        file.PublishedLocation = fullname;
+
+                        file.PublishedLocation = virtualFullname.Replace("~", "");
                     }
                 }
                 return Ok(file);
